15bd5b9e0980e5e00d6c6db33089d425f0079276
AsutoshTessell <asutosh.behera@tessell.com>
2026-07-31 22:36:24 +0530

    sdf

--- gitleaks.js
+++ gitleaks.js
@@ -1,3 +1,4 @@
 // AWS-style key format triggers gitleaks
 const AWS_SECRET = "AKIAIOSFODNN7EXAMPLV";
 const stripeKey1 = "sk_live_51H8xExampleFakeKeyForDemoOnly1234567890";
+const stripeKey2 = "sk_live_51H8xExampleFakeKeyForDemoOnly1234567891";
